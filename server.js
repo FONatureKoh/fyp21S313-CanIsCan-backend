361c29ce3958c0bc459af6c9331d49b993b3c7d8
FONatureKoh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-02 20:51:39 +0800

    setting up basic server API

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    console.log("Route success");
+    res.json({message: "Server Load successful!"});
+});
+
+/* === All /users routes matters === */
+const userRouter = require("./routes/users")
+app.use("/users", userRouter)
+
+app.listen(5000);

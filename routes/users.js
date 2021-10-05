2339746ae9d6ed9a5520d50192c7ef69ef0d7b80
FONatureKoh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-05 12:50:04 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const dbconn = require("../models/db_model");
 
-router.get("/", (req, res) => {
-  res.send("User List");
+/* === Returns current users === */
+router.get("/list", (req, res) => {
+	dbconn.query('SELECT * FROM app_users', function (error, results, fields) {
+    if (error) {
+      res.send("MySQL error: " + error);
+    }
+    else {
+      res.send(results);
+    }
+  });
 });
 
 /* === All routes for /users/:username ===

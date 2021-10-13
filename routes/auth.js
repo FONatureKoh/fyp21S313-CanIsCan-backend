3fc271e7d86c83f57a938f6803570b4a4200d26f
FONatureKoh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-13 16:31:26 +0800

    Summary of implementing the token

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const dbconn = require("../models/db_model");
 
@@ -18,12 +19,38 @@ router.post("/login", (req, res) => {
   // res.send(req.body);
 	dbconn.query(sqlQuery, function (error, results, fields) {
     if (error) {
-      res.send("MySQL error: " + error);
+      res.status(404).send("MySQL error: " + error);
     }
     else {
-      res.send(results);
+      // Retrieve the username and usertype
+      const selectedUsername = results[0].username;
+      const selectedUsertype = results[0].usertype;
+
+      // Construct an object with those params
+      const userData = {
+        username: selectedUsername,
+        userType: selectedUsertype
+      };
+      
+      // Now we serialise things
+      const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
+      
+      res.json({ accessToken: accessToken })
+      // res.send(results);
     }
   });
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorisation'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(404);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userData) => {
+    if (err) return res.sendStatus(403)
+    req.body.username
+  })
+
+}
 
 module.exports = router;

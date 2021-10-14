7c3960c5e6faf754afea346925ec607d15ce5d8d
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-14 12:19:00 +0800

    Fixed undefined token

--- middleware/authTokenMiddleware.js
+++ middleware/authTokenMiddleware.js
@@ -3,7 +3,8 @@ const jwt = require("jsonwebtoken");
 module.exports = function authenticateToken (req, res, next) {
   const authHeader = req.headers['authorisation'];
   const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(404);
+
+  if (token == null) return res.status(404).json({ errorMsg: "Token Error please try again" });
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userData) => {
     if (err) return res.sendStatus(403);

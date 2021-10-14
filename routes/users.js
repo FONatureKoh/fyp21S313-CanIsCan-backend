e4abed48780e327be4d4f4531338262d57f19473
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-14 12:54:33 +0800

    Update users.js
    Now with Profile Management routes enabled

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const authTokenMiddleware = require("../middleware/authTokenMiddleware");
 const router = express.Router();
 const dbconn = require("../models/db_model");
+
+// Body parser
+router.use(express.json());
 
 /* */
 router.get("/list", (req, res) => {
@@ -14,6 +18,47 @@ router.get("/list", (req, res) => {
     }
   });
 });
+
+/****************************************************************************
+ * User profile Management																									*
+ ****************************************************************************
+ * userType: Restaurant General Manager, Restaurant Deliveries Manager,			*
+ * Restaurant Reservation Manager
+*/
+router
+  .route('/profilemanagement')
+	.get(authTokenMiddleware, (req, res) => {
+		// Get the userData from the access token
+		const {
+			username, userType
+		} = res.locals.userData;
+
+		// Construct a Switch to handle the sql query based on the userType
+		switch (userType) {
+			case "Restaurant General Manager":
+				// Then, we construct the sql query with the username in mind.
+				var sqlQuery = "SELECT username, first_name, last_name, phone_no "
+				sqlQuery += "FROM app_user JOIN restaurant_gm "
+				sqlQuery += `ON username=rgm_username WHERE rgm_username='${username}'`;
+
+				// Query the db and return the said fields to the frontend app
+				dbconn.query(sqlQuery, function (error, results, fields) {
+					if (error) {
+						res.status(400).send({ errorMsg: "MySQL error: " + error });
+					}
+					else {
+						res.status(200).send(results[0]);
+					}
+				})
+				break;
+		
+			default:
+				break;
+		}
+	})
+	.put(authTokenMiddleware, (req, res) => {
+		res.send();
+	});
 
 /* === All routes for /users/:username ===
 	Currently has get, put, delete

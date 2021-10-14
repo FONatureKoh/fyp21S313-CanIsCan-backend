c01a1962db6db7957fef98cb48f9ab19ffdf11ec
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-14 10:45:27 +0800

    Update auth.js
    Route now properly contains status replies and errormessages

--- routes/auth.js
+++ routes/auth.js
@@ -7,47 +7,60 @@ const dbconn = require("../models/db_model");
 const authenticateToken = require("../middleware/authTokenMiddleware");
 
 router.use(express.json())
-/************************************
- * Authentication function
- * 
+/************************************************************************************
+ * Authentication function                                                          *
+ ************************************************************************************
+ * Authentication function to send the accessToken back to the frontend client
  */
 
 router.post("/login", (req, res) => {
-  var username = req.body.username;
-  var password = req.body.password;
+  console.log(req.body);
+  // First we get the two values from the req body
+  const {
+    username, password
+  } = req.body;
 
-  // Authenticate the user
-  var sqlQuery = `SELECT username, user_type FROM app_user `;
-  sqlQuery += `WHERE username='${username}' AND user_password='${password}'`;
+  // If the two authentication values do not exist, then return error response
+  if (username == null || password == null) {
+    res.status(400).json({ errorMsg: "Username and Password cannot be blank" });
+  }
+  // If username and password found, then proceed with the query
+  else {
+    // Create query to authenticate user
+    var sqlQuery = `SELECT username, user_type FROM app_user `;
+    sqlQuery += `WHERE username='${username}' AND user_password='${password}'`;
 
-  console.log(sqlQuery);
-  // console.log(req.body);
-  // res.send(req.body);
-	dbconn.query(sqlQuery, function (error, results, fields) {
-    if (error) {
-      res.status(400).send("MySQL error: " + error);
-    }
-    else {
-      // Retrieve the username and usertype
-      const selectedUsername = results[0].username;
-      const selectedUsertype = results[0].user_type;
+    // Query to auth the user. If found, proceed, otherwise throw 400 Bad Request.
+    dbconn.query(sqlQuery, function (error, results, fields) {
+      console.log(results);
+      if (error) {
+        res.status(400).send({ errorMsg: "MySQL error: " + error });
+      }
+      else if (results.length > 0) {
+        // Retrieve the username and usertype
+        const selectedUsername = results[0].username;
+        const selectedUsertype = results[0].user_type;
 
-      // Construct an object with those params
-      const userData = {
-        username: selectedUsername,
-        userType: selectedUsertype
-      };
-      
-      // Now we serialise things
-      const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
-      
-      // Send the response back to front end app
-      res.status(200).json({
-        accessToken: accessToken,
-        userType: selectedUsertype
-      })
-    }
-  });
+        // Construct an object with those params
+        const userData = {
+          username: selectedUsername,
+          userType: selectedUsertype
+        };
+        
+        // Now we serialise things
+        const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
+        
+        // Send the response back to front end app
+        res.status(200).json({
+          accessToken: accessToken,
+          userType: selectedUsertype
+        })
+      }
+      else {
+        res.status(400).send({ errorMsg: "User not found. Please try again" });
+      }
+    });
+  }
 });
 
 router.get("/username", authenticateToken, (req, res) => {

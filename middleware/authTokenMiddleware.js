6bcabc9191a8e4a92dbdcf9546cd70447a6b8e9d
FONatureKoh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-14 16:49:42 +0800

    Updated status code for authTokenMiddleware.js

--- middleware/authTokenMiddleware.js
+++ middleware/authTokenMiddleware.js
@@ -4,7 +4,7 @@ module.exports = function authenticateToken (req, res, next) {
   const authHeader = req.headers['authorisation'];
   const token = authHeader && authHeader.split(' ')[1]
 
-  if (token == null) return res.status(404).json({ errorMsg: "Token Error please try again" });
+  if (token == null) return res.status(400).json({ errorMsg: "Token Error please try again" });
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userData) => {
     if (err) return res.sendStatus(403);

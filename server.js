dcee79729f53598ba15b166a41a7d3ba0e18d59f
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-15 12:43:52 +0800

    Updated CORS config

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ const cors = require('cors');
 const path = require('path');
 
 app.use(express.static("public"));
-app.use(cors());
+
+// CORS matters
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   console.log("Route success");

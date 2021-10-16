1488c5ba64d2b5839e41ace0338fa5740b937166
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-16 15:51:19 +0800

    Update authTokenMiddleware.js

--- middleware/authTokenMiddleware.js
+++ middleware/authTokenMiddleware.js
@@ -10,7 +10,7 @@ module.exports = function authenticateToken (req, res, next) {
   if (token == null) return res.status(400).json({ errorMsg: "Token Error please try again" });
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userData) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.status(403).json({ errorMsg: "Invalid Token, what are you trying to do?" });
 
     res.locals.userData = userData;
     next();

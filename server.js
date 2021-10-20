e007b904db6040fe3ee7bdd9e5cda27889e83596
Thomas Koh <43998923+FONatureKoh@users.noreply.github.com>
2021-10-20 17:44:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const { google } = require('googleapis');
+const nodemailer = require('nodemailer');
 
 app.use(express.static("public"));
 
@@ -60,8 +62,71 @@ app.use("/restaurant", restaurantRouter);
 
 /* === All /register routes matters === */
 const registerRouter = require("./routes/register");
+
 app.use("/register", registerRouter);
 
+/*****************************************************************************
+ * Test for sending an email                                                 *
+ *****************************************************************************
+ */
+// Basic config for the google api
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.EMAIL_CLIENT_ID, 
+  process.env.EMAIL_CLIENT_SECRET, 
+  process.env.REDIRECT_URL
+);
+
+oAuth2Client.setCredentials({
+  refresh_token: process.env.EMAIL_REFRESH_TOKEN
+})
+
+// Creating an async function for sending emails
+async function sendMail() {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+
+    const transport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'cancanfoodapp@gmail.com',
+        clientId: process.env.EMAIL_CLIENT_ID,
+        clientSecret: process.env.EMAIL_CLIENT_SECRET,
+        refreshToken: process.env.EMAIL_REFRESH_TOKEN,
+        accessToken: accessToken
+      }
+    });
+
+    const mailOptions = {
+      from: 'Administrator <cancanfoodapp@gmail.com>',
+      to: 'fonaturekoh@outlook.sg',
+      subject: 'This is a test for the gmail API',
+      test: 'Hello world',
+      html: '<h1>Hello world </h1>'
+    };
+
+    const result = await transport.sendMail(mailOptions);
+    return result;
+  }
+  catch (error) {
+    return error;
+  }
+
+}
+
+app.get('/testemail', (req, res) => {
+  sendMail()
+    .then(result => {
+      console.log(result);
+      res.status(200).json({ api_msg: result });
+    })
+    .catch((error) => console.log(error.message));
+})
+
+/*****************************************************************************
+ * Always end with what is right below this, listen                          *
+ *****************************************************************************
+ */ 
 app.listen(5000, () => {
   console.log("Listening on port 5000!")
 });
